8046f3b3d7a5bcf9c373bef46a2a173a7dc5897e
thrown <107180973+linwuerliu@users.noreply.github.com>
2024-01-26 19:02:50 +0800

    Add files via upload

---
+++ xxpp-backend/sql/create_table.sql
@@ -0,0 +1,88 @@
+# 数据库初始化
+
+
+-- 创建库
+create database if not exists yupi;
+
+-- 切换库
+use yupi;
+
+# 用户表
+create table user
+(
+    id           bigint auto_increment comment 'id'
+        primary key,
+    username     varchar(1024)                      null comment '用户昵称',
+    userAccount  varchar(256)                       null comment '账号',
+    avatarUrl    varchar(1024)                      null comment '用户头像',
+    gender       tinyint                            null comment '性别',
+    userPassword varchar(512)                       not null comment '密码',
+    phone        varchar(128)                       null comment '电话',
+    email        varchar(512)                       null comment '邮箱',
+    userStatus   int      default 0                 not null comment '状态 0 - 正常',
+    createTime   datetime default CURRENT_TIMESTAMP null comment '创建时间',
+    updateTime   datetime default CURRENT_TIMESTAMP null on update CURRENT_TIMESTAMP,
+    isDelete     tinyint  default 0                 not null comment '是否删除',
+    userRole     int      default 0                 not null comment '用户角色 0 - 普通用户 1 - 管理员',
+    planetCode   varchar(512)                       null comment '星球编号',
+    tags         varchar(1024)                      null comment '标签列表'
+)
+    comment '用户' charset = utf8;
+
+-- 用户表
+
+
+-- 队伍表
+create table team
+(
+    id          bigint auto_increment comment 'id' primary key,
+    name        varchar(256)       not null comment '队伍名称',
+    description varchar(1024) null comment '描述',
+    maxNum      int      default 1 not null comment '最大人数',
+    expireTime  datetime null comment '过期时间',
+    userId      bigint comment '用户id（队长 id）',,
+    status      int      default 0 not null comment '0 - 公开，1 - 私有，2 - 加密',
+    password    varchar(512) null comment '密码',
+    createTime  datetime default CURRENT_TIMESTAMP null comment '创建时间',
+    updateTime  datetime default CURRENT_TIMESTAMP null on update CURRENT_TIMESTAMP,
+    isDelete    tinyint  default 0 not null comment '是否删除'
+) comment '队伍' charset = utf8;
+
+-- 用户队伍关系
+create table user_team
+(
+    id         bigint auto_increment comment 'id'
+        primary key,
+    userId     bigint comment '用户id',
+    teamId     bigint comment '队伍id',
+    joinTime   datetime null comment '加入时间',
+    createTime datetime default CURRENT_TIMESTAMP null comment '创建时间',
+    updateTime datetime default CURRENT_TIMESTAMP null on update CURRENT_TIMESTAMP,
+    isDelete   tinyint  default 0 not null comment '是否删除'
+) comment '用户队伍关系' charset = utf8;
+
+
+-- 标签表（可以不创建，因为标签字段已经放到了用户表中）
+create table tag
+(
+    id         bigint auto_increment comment 'id'
+        primary key,
+    tagName    varchar(256) null comment '标签名称',
+    userId     bigint null comment '用户 id',
+    parentId   bigint null comment '父标签 id',
+    isParent   tinyint null comment '0 - 不是, 1 - 父标签',
+    createTime datetime default CURRENT_TIMESTAMP null comment '创建时间',
+    updateTime datetime default CURRENT_TIMESTAMP null on update CURRENT_TIMESTAMP,
+    isDelete   tinyint  default 0 not null comment '是否删除',
+    constraint uniIdx_tagName
+        unique (tagName)
+) comment '标签' charset = utf8;
+
+create index idx_userId
+    on tag (userId);
+
+
+# 导入示例用户
+INSERT INTO `user-center`.user (username, userAccount, avatarUrl, gender, userPassword, phone, email, userStatus, createTime, updateTime, isDelete, userRole, planetCode) VALUES ('鱼皮', 'yupi', 'https://himg.bdimg.com/sys/portraitn/item/public.1.e137c1ac.yS1WqOXfSWEasOYJ2-0pvQ', null, 'b0dd3697a192885d7c055db46155b26a', null, null, 0, '2023-08-06 14:14:22', '2023-08-06 14:39:37', 0, 1, '1');
+
+
---
+++ xxpp-backend/src/main/java/com/yupi/usercenter/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.yupi.usercenter.mapper;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.yupi.usercenter.model.domain.User;
+
+/**
+ * @Entity generator.domain.User
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
